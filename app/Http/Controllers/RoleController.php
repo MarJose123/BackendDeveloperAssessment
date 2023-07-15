f649960d9f9841d29e775791b186f72e6ae6fc84
Marjose123 <hypernate1@gmail.com>
2023-07-15 18:33:42 +0800

    WIP

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -31,11 +31,36 @@ class RoleController extends Controller
             'code' => Response::HTTP_UNPROCESSABLE_ENTITY,
             'message' => 'role_name address and permissions is required.'
         ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        /*
+         * Check if the permission provided is in array format
+         */
+        if(!is_array($request->permissions)) return response()->json([
+            'status' => 'failed',
+            'code' => Response::HTTP_UNPROCESSABLE_ENTITY,
+            'message' => 'Permissions must be in array format.'
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
 
         /*
          * Creation and Storing the DB
          */
+       $createdRole =  Role::create(['name' => $request->role_name])
+            ->givePermissionTo(Permission::whereIn('id', $request->permissions)->get());
 
+       if(!$createdRole) return response()->json([
+           'status' => 'failed',
+           'code' => Response::HTTP_CREATED,
+           'message' =>  'Resources not created for unknown issue.',
+           'count' => 0,
+           'data' => null
+       ], Response::HTTP_CREATED);
+
+       return response()->json([
+           'status' => 'success',
+           'code' => Response::HTTP_OK,
+           'message' =>  'New role has been successfully added.',
+           'count' => Permission::count(),
+           'data' => $createdRole
+       ], Response::HTTP_OK);
 
     }
     public function permissionList()
